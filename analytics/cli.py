cf623a71306fe2bd53ff9d907d15c05bd1110f9b
João Neto <joao.alexandre@cesar.org.br>
2021-09-05 11:29:46 -0300

    analytics: Rename and improve a lot of things

--- analytics/cli.py
+++ analytics/cli.py
@@ -8,6 +8,16 @@ from network.mqtt import MQTT
 from network.publisher import MessagePublisher
 import click
 import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+import csv
+import numpy as np
+
+from skmultiflow.data import SEAGenerator
+from skmultiflow.trees import HoeffdingTreeClassifier
+from skmultiflow.bayes import NaiveBayes
+from skmultiflow.evaluation import EvaluatePrequential
+from skmultiflow.data import DataStream
 
 # Default to publish respecting original timestamp
 publish_delay_in_seconds = 5
@@ -93,25 +103,24 @@ def stop():
 def drift():
     publisher.publish_detected_drift()
 
-
-@main.command()
-@click.option('-f', '--factor', 'factor', default='5')
-@click.option('-n', '--number', 'number', default='100000')
-@click.option('-c', '--chunk', 'chunk', default='100000')
-@click.option('-f', '--factor', 'factor', default='5')
-def botnet(factor, number, chunk):
+def publish_botnet_data(factor='5', number='100000', chunk='100000'):
     reader = pd.read_csv(
-        'botnet_dataset_1.csv',
+        '../test.csv',
         usecols=['stime', 'proto', 'saddr', 'sport', 'daddr', 'dport', 'bytes', 'state'],
-        dtype={'stime': float, 'proto': str, 'saddr': str, 'sport': int, 'daddr': str, 'dport': int, 'bytes': int, 'state': str},
+        dtype={'stime': float, 'proto': str, 'saddr': str, 'daddr': str, 'bytes': int, 'state': str},
         nrows=int(number),
+        # converters={"sport": lambda x: int(str(x), 16)},
         chunksize=100000
     )
     now = time.time()
     count = 0
     for df in reader:
         df = df.dropna()
+        df = df[df['sport'] != '0x0303']
+        df = df[df['dport'] != '0x5000']
+
         for index, row in df.iterrows():
+            print(row)
             count = count + 1
             data = {
                 "timestamp": row['stime'],
@@ -128,6 +137,100 @@ def botnet(factor, number, chunk):
     elapsed = time.time() - now
     print('published {} events in {} seconds/minutes'.format(count, elapsed))
     print('throughput: {}'.format(count / elapsed))
+
+@main.command()
+@click.option('-n', '--number', 'number', default='100000')
+@click.option('-c', '--chunk', 'chunk', default='100000')
+@click.option('-f', '--factor', 'factor', default='5')
+def botnet(factor='5', number='100000', chunk='100000'):
+    publish_botnet_data(factor, number, chunk)
+
+@main.command()
+def cleanup():
+    reader = pd.read_csv(
+        'botnet_dataset_1.csv',
+        usecols=['stime', 'proto', 'saddr', 'sport', 'daddr', 'dport', 'bytes', 'state'],
+        dtype={'stime': float, 'proto': str, 'saddr': str, 'sport': int, 'daddr': str, 'dport': int, 'bytes': int, 'state': str},
+        nrows=int(number),
+        chunksize=100000
+    )
+    for df in reader:
+        df = df.dropna()
+        for index, row in df.iterrows():
+            print(row)
+
+@main.command()
+def run():
+    factors = ['0.01', '0.001', '0.0001', '0.00001', '0.000001']
+    for f in factors:
+        timestamp = datetime.now()
+        print('{} - botnet workload: factor {}'.format(timestamp, f))
+        publish_botnet_data(f, '500000')
+
+        # idle time
+        timestamp = datetime.now()
+        print('{} - botnet workload: idle time'.format(timestamp))
+        time.sleep(15)
+
+@main.command()
+@click.option('-n', '--number', 'number', default='100000')
+def generate(number):
+    df = pd.read_csv('../results/profiler/profiler-execution-0-workload-ddos-10s.jar-2021-07-3121:36:44.591528.csv')
+    bandwidth_std = df['bandwidth'].std()
+    bandwidth_mean = df['bandwidth'].mean()
+    cpu_std = df['cpu'].std()
+    cpu_mean = df['cpu'].mean()
+    cep_latency_std = df['cepLatency'].std()
+    cep_latency_mean = df['cepLatency'].mean()
+    memory_std = df['memory'].std()
+    memory_mean = df['memory'].mean()
+    rtt_std = df['rtt'].std()
+    rtt_mean = df['rtt'].mean()
+
+    bandwidth = np.random.normal(loc=bandwidth_mean, scale=bandwidth_std, size=int(number))
+    cpu = np.random.normal(loc=cpu_mean, scale=cpu_std, size=int(number))
+    cep_latency = np.random.normal(loc=cep_latency_mean, scale=cep_latency_std, size=int(number))
+    memory = np.random.normal(loc=memory_mean, scale=memory_std, size=int(number))
+    rtt = np.random.normal(loc=rtt_mean, scale=rtt_std, size=int(number))
+
+    result = []
+
+    dt = datetime(2021, 7, 1)
+    end = datetime(2021, 7, 31, 23, 59, 59)
+    step = timedelta(seconds=20)
+
+    while dt < end:
+        result.append(dt.strftime('%Y-%m-%d %H:%M:%S'))
+        dt += step
+
+
+    header = ['bandwidth', 'cepLatency', 'cpu', 'memory', 'rtt', 'timestamp']
+    with open('random_data_test.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+
+        # print(type(bandwidth))
+
+        for i in range(bandwidth.shape[0]):
+            instance = [ bandwidth[i], cep_latency[i], cpu[i], memory[i], rtt[i], result[i]]
+            # print(instance)
+            writer.writerow(instance)
+
+@main.command()
+def evaluate():
+    df = dataset = pd.read_csv('../random_data_labeled.csv')
+    df['violated'] = df['violated'].astype(np.int64)
+    df = df.drop(columns=['timestamp'])
+    stream = DataStream(df)
+    ht = HoeffdingTreeClassifier()
+    nb = NaiveBayes()
+    evaluator = EvaluatePrequential(max_samples=100000,
+                                max_time=1000,
+                                show_plot=True,
+                                metrics=['accuracy', 'kappa'])
+
+    evaluator.evaluate(stream=stream, model=[ht, nb], model_names=['HT', 'NB'])
+
 
 @main.command()
 @click.option('-f', '--factor', 'factor', default='5')
@@ -187,24 +290,6 @@ def weather(factor):
         #     count = 0
     elapsed = time.time() - now
     print('published {} events in {} seconds/minutes'.format(count, elapsed))
-
-
-
-
-
-
-# saída esperada:
-
-# Starting benchmark - <start_datetime>
-# Benchmark ended - <end_datetime>
-
-# X events published in a throughput of seconds/minutes
-# X resulting events received
-
-# Time interval of <end_datetime> - <start_datetime>
-
-
-
 
 if __name__ == "__main__":
     main()
