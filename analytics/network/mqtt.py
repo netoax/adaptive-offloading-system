bd29ae285ebfe7975f8e83abab69796a35ddbba3
João Neto <joao.alexandre@cesar.org.br>
2021-11-09 01:15:54 -0300

    network: Improve MQTT connection

--- analytics/network/mqtt.py
+++ analytics/network/mqtt.py
@@ -8,7 +8,9 @@ class MQTT():
 
     def start(self):
         self.client.on_connect = self.on_connected
-        self.client.connect(self.hostname, self.port)
+        # self.client.on_disconnect = self.on_disconnect
+        self.client.connect_async(self.hostname, self.port)
+        self.loop_start()
 
     def stop(self):
         self.client.disconnect()
@@ -16,8 +18,15 @@ class MQTT():
     def loop_forever(self):
         self.client.loop_forever()
 
+    def loop_start(self):
+        self.client.loop_start()
+
     def on_connected(self, client, userdata, flags, rc):
-        print("Connected to MQTT broker")
+        # print("Connected to MQTT broker")
+        pass
+
+    def on_disconnected(self, client, userdata, flags, rc):
+        print("Disconnected from broker")
 
     def publish(self, topic, payload):
         self.client.publish(topic, payload)
